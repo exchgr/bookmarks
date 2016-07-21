79362e45d61d32e94406ab7d9b83e4c239845fca
Elle Mundy <exchgr@icloud.com>
2016-07-21 19:10:43 -0400

    Adds bookmarks controller, routes, index view. Changes title.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookmarks, only: [:index]
+
+  root to: "bookmarks#index"
 end
