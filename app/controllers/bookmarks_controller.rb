112449afa2b3f907668f1f8579d9905309effbb6
Elle Mundy <exchgr@icloud.com>
2016-07-21 22:55:04 -0400

    Saves or shows validation errors on bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,4 +6,20 @@ class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to root_path
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params[:bookmark].permit(:title, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bookmarks, only: [:index, :new]
+  resources :bookmarks, only: [:index, :new, :create]
 
   root to: "bookmarks#index"
 end
