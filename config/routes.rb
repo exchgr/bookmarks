f0caade2c4f7e9912a7150500d179e851fa2614d
Elle Mundy <exchgr@icloud.com>
2016-07-28 15:37:39 -0400

    Delete bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bookmarks, only: [:index, :new, :create]
+  resources :bookmarks, only: [:index, :new, :create, :destroy]
 
   root to: "bookmarks#index"
 end
